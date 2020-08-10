57e4fa04fd669ad14b92b0cfbe93e2b2cf0ded81
arevathi69 <66195488+arevathi69@users.noreply.github.com>
2020-08-10 12:42:50 +0530

    Created switch1.c
    This program displays default printf statement on the screen as computer only knows 0 and 1

---
+++ switch1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	
+	
+	
+	printf("enter the option \n1.0 \n2.1");
+	int option=0;
+	scanf("%d",&option);
+	switch(option)
+	{
+		case 1:
+			printf("0\n");
+			break;
+		case 2:
+		    printf("1\n");
+			break;
+		default:printf("Choice other than 1 and 2\n");
+		    		
+		
+	}
+}
